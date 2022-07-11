e3f78c5b8d0ddac4b07dd7f7fbc807019af09e10
DESKTOP-931QLKQ\jongro-hi <dbcks4646@gmail.com>
2022-07-11 16:53:50 +0900

    20220711 16:52 김유찬 메인페이지 폼 완성
    메인페이지 출력 및 등록(데이터 입력x)

--- bukkeubook-final-project/src/main/java/com/bukkeubook/book/main/controller/MainController.java
+++ bukkeubook-final-project/src/main/java/com/bukkeubook/book/main/controller/MainController.java
@@ -1,21 +1,13 @@
 package com.bukkeubook.book.main.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-	@GetMapping(value = {"/", "/main"})
-    public String main() {
-
-        return "main/main";
-    }
-
-    @PostMapping(value="/")
-    public String redirectMain() {
-
-        return "redirect:/";
-    }
+	@RequestMapping("/")
+	public String main() {
+		return "main";
+	}
 }
