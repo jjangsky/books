8e5624506138998aa3b52641e8900d0dcff451d3
@youn3kd3 <youn3kd3@gmail.com>
2022-07-12 17:55:19 +0900

    20220712 윤큰담 도서정보수정 기능추가

--- bukkeubook-final-project/src/main/java/com/bukkeubook/book/books/model/repository/BookRepository.java
+++ bukkeubook-final-project/src/main/java/com/bukkeubook/book/books/model/repository/BookRepository.java
@@ -3,7 +3,9 @@ package com.bukkeubook.book.books.model.repository;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Repository;
 
 import com.bukkeubook.book.books.model.entity.Book;
@@ -25,6 +27,9 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
 
 	List<Book> findBookByNo(String no);
 
+
+		
+		
 	
 	
 	
