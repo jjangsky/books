a960623b40d5160070e89193dcbc73b8bf6355d6
김효진 <khjhj4884@gmail.com>
2022-07-14 13:16:18 +0900

    20220714  김효진 사원상세조회 , 퇴사직원 상세조회 기능 완성 empDTO ADDRESS 부분 변경 퇴사직원상세조회 화면  CSS 추가

--- bukkeubook-final-project/src/main/java/com/bukkeubook/book/manage/model/entity/DeptAndEmp.java
+++ bukkeubook-final-project/src/main/java/com/bukkeubook/book/manage/model/entity/DeptAndEmp.java
@@ -11,38 +11,41 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /* TBL_DEPT 테이블에 매칭될 Dept 엔티티 클래스도 만들어 보기 */
-@Entity(name="DeptAndDept")
+@Entity
 @Table(name = "TBL_DEPT")
 public class DeptAndEmp implements Serializable {
 
-	private static final long serialVersionUID = 541840903128253305L;
+   private static final long serialVersionUID = 541840903128253305L;
 
-	/* DB 자료형 */
-	
-//	DEPT_CODE		NUMBER				부서코드
-//	DEPT_NAME		NVARCHAR2(31 CHAR)	부서명
-//	DEPT_REP_PHONE	VARCHAR2(15 BYTE)	대표번호
-	
-	@Id
-	@Column(name = "DEPT_CODE")
-	private int deptCode;
-	
-	@Column(name = "DEPT_NAME")
-	private String deptName;
-	
-	@Column(name = "DEPT_REP_PHONE")
-	private String deptRepPhone;
-	
-	@OneToMany(mappedBy ="dept")
-	private List<EmpAndDept> empList = new ArrayList<>();
+   /* DB 자료형 */
+   
+//   DEPT_CODE      NUMBER            부서코드
+//   DEPT_NAME      NVARCHAR2(31 CHAR)   부서명
+//   DEPT_REP_PHONE   VARCHAR2(15 BYTE)   대표번호
+   
+   @Id
+   @Column(name = "DEPT_CODE")   // PK
+   private int deptCode;
+   
+   @Column(name = "DEPT_NAME")
+   private String deptName;
+   
+   @Column(name = "DEPT_REP_PHONE")
+   private String deptRepPhone;
+   
+   @OneToMany(mappedBy ="dept")   // FK명
+   private List<EmpAndDept> empList = new ArrayList<>();
 
 	public DeptAndEmp() {
+		super();
 	}
 
-	public DeptAndEmp(int deptCode, String deptName, String deptRepPhone) {
+	public DeptAndEmp(int deptCode, String deptName, String deptRepPhone, List<EmpAndDept> empList) {
+		super();
 		this.deptCode = deptCode;
 		this.deptName = deptName;
 		this.deptRepPhone = deptRepPhone;
+		this.empList = empList;
 	}
 
 	public int getDeptCode() {
@@ -69,14 +72,22 @@ public class DeptAndEmp implements Serializable {
 		this.deptRepPhone = deptRepPhone;
 	}
 
+	public List<EmpAndDept> getEmpList() {
+		return empList;
+	}
+
+	public void setEmpList(List<EmpAndDept> empList) {
+		this.empList = empList;
+	}
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
 	@Override
 	public String toString() {
-		return "Dept [deptCode=" + deptCode + ", deptName=" + deptName + ", deptRepPhone=" + deptRepPhone + "]";
+		return "DeptAndEmp [deptCode=" + deptCode + ", deptName=" + deptName + ", deptRepPhone=" + deptRepPhone
+				+ ", empList=" + empList + "]";
 	}
-	
-	
+
 }
