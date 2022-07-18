6d71d0ebfdceb30ae8ab0c6166d0a5848d71638d
DESKTOP-931QLKQ\jongro-hi <dbcks4646@gmail.com>
2022-07-18 11:49:57 +0900

    20220718 김유찬 마이페이지 정보 수정 완료
    마이페이지 정보 수정 및 메인 페이지 사진, 전자결재 삭제

--- bukkeubook-final-project/src/main/java/com/bukkeubook/book/mypage/controller/MyInfoModifyController.java
+++ bukkeubook-final-project/src/main/java/com/bukkeubook/book/mypage/controller/MyInfoModifyController.java
@@ -1,11 +1,16 @@
 package com.bukkeubook.book.mypage.controller;
 
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.bukkeubook.book.manage.model.dto.EmpDTO;
 import com.bukkeubook.book.manage.model.dto.joinDTO.EmpAndDeptDTO;
 import com.bukkeubook.book.mypage.model.service.MyInfoModifyService;
 
@@ -32,6 +37,20 @@ public class MyInfoModifyController {
 		
 		mv.addObject("myInfo", myInfo);
 		mv.setViewName("mypage/mypageInfoModify");
+		
+		return mv;
+	}
+	
+	/* 마이페이지 개인 정보 수정하기 */
+	@PostMapping("/modifyInfo")
+	public ModelAndView modifyMyInfo(ModelAndView mv, EmpDTO emp, RedirectAttributes rttr, Locale locale) {
+		
+		int memberCode = 5;
+		System.out.println(emp);
+		myInfoModifyService.modifyInfoEmp(memberCode, emp);
+
+		rttr.addFlashAttribute("successMessage", "회원정보를 정상적으로 수정하였습니다.");
+		mv.setViewName("redirect:/");
 		
 		return mv;
 	}
--- bukkeubook-final-project/src/main/java/com/bukkeubook/book/mypage/model/service/MyInfoModifyService.java
+++ bukkeubook-final-project/src/main/java/com/bukkeubook/book/mypage/model/service/MyInfoModifyService.java
@@ -4,7 +4,9 @@ package com.bukkeubook.book.mypage.model.service;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.bukkeubook.book.manage.model.dto.EmpDTO;
 import com.bukkeubook.book.manage.model.dto.joinDTO.EmpAndDeptDTO;
 import com.bukkeubook.book.manage.model.entity.EmpAndDept;
 import com.bukkeubook.book.mypage.model.repository.EmployeeRepository;
@@ -27,6 +29,21 @@ public class MyInfoModifyService {
 		EmpAndDept myInfo = employeeRepository.findById(memberCode).get();
 		
 		return modelMapper.map(myInfo, EmpAndDeptDTO.class);
+	}
+
+	/* 마이페이지 개인정보 수정하기 */
+	@Transactional
+	public void modifyInfoEmp(int memberCode, EmpDTO emp) {
+		
+		
+		EmpAndDept myInfo = employeeRepository.findById(memberCode).get();
+		myInfo.setEmpEmail(emp.getEmpEmail());
+		myInfo.setEmpAddress(emp.getEmpAddress());
+		myInfo.setEmpDAddress(emp.getEmpDAddress());
+		myInfo.setEmpPhone1(emp.getEmpPhone1());
+		myInfo.setEmpPhone2(emp.getEmpPhone2());
+		myInfo.setEmpPhone3(emp.getEmpPhone3());
+		
 	}
 	
 
