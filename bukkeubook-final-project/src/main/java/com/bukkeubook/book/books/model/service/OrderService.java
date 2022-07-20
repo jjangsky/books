50f75098c42e19cf2309d3c19c0136dfa1690582
DESKTOP-SUOBOJT\Jang <12161782@inha.edu>
2022-07-20 14:26:35 +0900

    2022-07-20 ajax 검색 처리(orderRegist 쪽만) - 장준혁

--- bukkeubook-final-project/src/main/java/com/bukkeubook/book/books/model/service/OrderService.java
+++ bukkeubook-final-project/src/main/java/com/bukkeubook/book/books/model/service/OrderService.java
@@ -3,6 +3,7 @@ package com.bukkeubook.book.books.model.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.transaction.Transactional;
 
@@ -13,10 +14,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import com.bukkeubook.book.books.model.dto.BookDTO;
 import com.bukkeubook.book.books.model.dto.OrderAndEmpAndBookDTO;
 import com.bukkeubook.book.books.model.dto.OrderListDTO;
+import com.bukkeubook.book.books.model.entity.Book;
 import com.bukkeubook.book.books.model.entity.OrderAndEmpAndBook;
 import com.bukkeubook.book.books.model.entity.OrderList;
+import com.bukkeubook.book.books.model.repository.BookRepository;
 import com.bukkeubook.book.books.model.repository.OrderRepository;
 import com.bukkeubook.book.books.model.repository.SimpleOrderRepository;
 import com.bukkeubook.book.common.paging.SelectCriteria;
@@ -26,12 +30,14 @@ public class OrderService {
 
 	private final OrderRepository orderRepository;
 	private final SimpleOrderRepository simpleOrderRepository;
+	private final BookRepository bookRepository;
 	private final ModelMapper modelMapper;
 	
 	@Autowired
-	public OrderService(OrderRepository orderRepository, SimpleOrderRepository simpleOrderRepository, ModelMapper modelMapper) {
+	public OrderService(OrderRepository orderRepository, SimpleOrderRepository simpleOrderRepository, BookRepository bookRepository, ModelMapper modelMapper) {
 		this.orderRepository = orderRepository;
 		this.simpleOrderRepository = simpleOrderRepository;
+		this.bookRepository = bookRepository;
 		this.modelMapper = modelMapper;
 	}
 	
@@ -54,7 +60,7 @@ public class OrderService {
 		int count = selectCriteria.getLimit();
 		String searchValue = selectCriteria.getSearchValue();
 
-		Pageable paging = PageRequest.of(index, count, Sort.by("orderNo").descending());
+		Pageable paging = PageRequest.of(index, count, Sort.by("orderDate").descending());
 
 		List<OrderAndEmpAndBook> orderHistoryList = new ArrayList<OrderAndEmpAndBook>();
 		if(searchValue != null) {
@@ -79,6 +85,30 @@ public class OrderService {
 	public void registOrder(OrderListDTO order) {
 		
 		simpleOrderRepository.save(modelMapper.map(order, OrderList.class));
+	}
+
+	public List<BookDTO> searchOrderList(String searchCondition, String searchValue) {
+		
+		List<Book> bookList = new ArrayList<Book>();
+		if(searchValue != null) {
+
+			if("name".equals(searchCondition)) {
+				bookList = bookRepository.findByNameContaining(searchValue, Sort.by("no"));
+			}
+
+			if("author".equals(searchCondition)) {
+				bookList = bookRepository.findByAuthorContaining(searchValue, Sort.by("no"));
+			}
+			
+			if("no".equals(searchCondition)) {
+				bookList = bookRepository.findByNoContaining(searchValue, Sort.by("no"));
+			}
+		} else {
+			bookList = bookRepository.findAll(Sort.by("no"));
+		}
+
+		/* 자바의 Stream API와 ModelMapper를 이용하여 entity를 DTO로 변환 후 List<MenuDTO>로 반환 */
+		return bookList.stream().map(Book -> modelMapper.map(Book, BookDTO.class)).collect(Collectors.toList());
 	}
 
 }
