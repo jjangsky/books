c96b5b2aed261710c45f42c9b63a8b1a2068f8b2
김효진 <khjhj4884@gmail.com>
2022-07-25 00:33:15 +0900

    20220725 김효진 사원 퇴사기능 완성

--- bukkeubook-final-project/src/main/java/com/bukkeubook/book/manage/model/repository/OriginalEmpRepository.java
+++ bukkeubook-final-project/src/main/java/com/bukkeubook/book/manage/model/repository/OriginalEmpRepository.java
@@ -5,11 +5,18 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.bukkeubook.book.manage.model.dto.EmpDTO;
 import com.bukkeubook.book.manage.model.entity.Emp;
 
 public interface OriginalEmpRepository extends JpaRepository<Emp, Integer>{
 
 	List<Emp> findMemberByempNo(int empNo);
+
+
+	EmpDTO findByEmpEndDate(EmpDTO emp);
+
+
+	Emp findByEmpNo(int empNo);
 
 //	@Query(value = "SELECT SEQ_EMP_NO.NEXTVAL FROM DUAL", nativeQuery = true)
 //	int findCurrentSeqEmpNo();
